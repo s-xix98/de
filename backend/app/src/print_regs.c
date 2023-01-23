8cb9dd92e2348c21110bada09bf40efa81cfb89b
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-01-23 22:36:55 +0900

    init de, show regs

---
+++ backend/app/src/print_regs.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/user.h>
+
+#include "x_ptrace.h"
+
+void print_regs(pid_t pid) {
+  struct user_regs_struct regs;
+
+  x_ptrace_get_register_info(pid, &regs);
+
+  printf("--- print_registers ---\n");
+  printf("RAX : 0x%llx\n", regs.rax);
+  printf("RBX : 0x%llx\n", regs.rbx);
+  printf("RCX : 0x%llx\n", regs.rcx);
+  printf("RDX : 0x%llx\n", regs.rdx);
+  printf("RSI : 0x%llx\n", regs.rsi);
+  printf("RDI : 0x%llx\n", regs.rdi);
+  printf("RBP : 0x%llx\n", regs.rbp);
+  printf("RSP : 0x%llx\n", regs.rsp);
+  printf("RIP : 0x%llx\n", regs.rip);
+  printf("R8  : 0x%llx\n", regs.r8);
+  printf("R9  : 0x%llx\n", regs.r9);
+  printf("R10 : 0x%llx\n", regs.r10);
+  printf("R11 : 0x%llx\n", regs.r11);
+  printf("R12 : 0x%llx\n", regs.r12);
+  printf("R13 : 0x%llx\n", regs.r13);
+  printf("R14 : 0x%llx\n", regs.r14);
+  printf("R15 : 0x%llx\n", regs.r15);
+  printf("--- --- ---\n");
+  printf("\n");
+}
