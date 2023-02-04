8a82c81cfd6bce107092b80d495853364e88337a
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-02-05 08:36:18 +0900

    socket io connect to backend python

--- frontend/src/components/RegsArea.tsx
+++ frontend/src/components/RegsArea.tsx
@@ -1,7 +1,14 @@
 import { RegType } from '../features/regs/types/RegType';
 import { ShowRegs } from '../features/regs/components/ShowRegs';
+import { io } from 'socket.io-client';
 
 export const RegsArea = () => {
+  const socket = io('http://localhost:8000');
+
+  socket.on('connect', () => {
+    console.log('socket connect', socket.connect());
+  });
+
   const regsArr: Array<RegType> = [
     { name: 'RAX', val: 0 },
     { name: 'RBX', val: 1 },
