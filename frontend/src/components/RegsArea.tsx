21394bac7890aade514cce243e8e76cde1c2eaed
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-02-06 05:48:21 +0900

    backend retrun regs.json

--- frontend/src/components/RegsArea.tsx
+++ frontend/src/components/RegsArea.tsx
@@ -2,17 +2,33 @@ import { RegType } from '../features/regs/types/RegType';
 import { ShowRegs } from '../features/regs/components/ShowRegs';
 import { io } from 'socket.io-client';
 
-export const RegsArea = () => {
-  const socket = io('http://localhost:8000');
+const socket = io('http://localhost:8000');
 
+export const RegsArea = () => {
   socket.on('connect', () => {
     console.log('socket connect', socket.connect());
   });
 
-  const regsArr: Array<RegType> = [
+  let regsArr: Array<RegType> = [
     { name: 'RAX', val: 0 },
     { name: 'RBX', val: 1 },
   ];
 
-  return <ShowRegs regs={regsArr} />;
+  const clickAct = () => {
+    const res = socket.emit('get_regs');
+    console.log('');
+    console.log('CLICK ACT', res);
+    console.log('');
+  };
+
+  socket.on('get_regs', (res) => {
+    console.log('SOCKET.ON GET_REGS', res);
+  });
+
+  return (
+    <div>
+      <button onClick={clickAct}>button</button>
+      <ShowRegs regs={regsArr} />
+    </div>
+  );
 };
