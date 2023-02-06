7647052f64acd8a306db55e3d9ef96bfbf6e9336
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-02-06 09:30:34 +0900

    ShowRegs change radix

--- frontend/src/features/regs/components/ShowRegs.tsx
+++ frontend/src/features/regs/components/ShowRegs.tsx
@@ -1,18 +1,54 @@
+import { useState } from 'react';
 import { RegType } from '../types/RegType';
 
-const ShowReg = ({ reg }: { reg: RegType }) => {
+const ShowReg = ({
+  reg,
+  radix,
+  radixPrefix,
+}: {
+  reg: RegType;
+  radix: number;
+  radixPrefix: string;
+}) => {
   return (
     <p>
-      {reg.name} : {reg.val}
+      {reg.name} : {radixPrefix} {reg.val.toString(radix)}
     </p>
   );
 };
 
 export const ShowRegs = ({ regs }: { regs: Array<RegType> }) => {
+  const [radix, setRadix] = useState(10);
+  const [radixPrefix, setRadixPrefix] = useState('');
+
+  const changeRadix = () => {
+    const prevRadix = radix;
+
+    if (radix == 10) {
+      setRadix(16);
+      setRadixPrefix('0x');
+    } else if (radix == 16) {
+      setRadix(2);
+      setRadixPrefix('0b');
+    } else {
+      setRadix(10);
+      setRadixPrefix('');
+    }
+
+    console.log('changeRadix', prevRadix, '->', radix);
+  };
+
   return (
     <div>
+      {regs.length != 0 && <button onClick={changeRadix}>Change Radix</button>}
+      {regs.length != 0 && <p>Radix {radix}</p>}
       {regs.map((reg, idx) => (
-        <ShowReg reg={reg} key={idx}></ShowReg>
+        <ShowReg
+          reg={reg}
+          radix={radix}
+          radixPrefix={radixPrefix}
+          key={idx}
+        ></ShowReg>
       ))}
     </div>
   );
