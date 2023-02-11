a06d8ca8e0c7e21122e7dc680624a5a4929dce69
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-02-11 09:23:25 +0900

    show code

--- frontend/src/components/CodeArea.tsx
+++ frontend/src/components/CodeArea.tsx
@@ -1,7 +1,10 @@
-export const CodeArea = () => {
+import { ShowCode } from '../features/code/components/ShowCode';
+
+export const CodeArea = ({ codeArr }: { codeArr: string[] }) => {
   return (
     <div>
       <h1>CodeArea</h1>
+      <ShowCode codeArr={codeArr} />
     </div>
   );
 };
